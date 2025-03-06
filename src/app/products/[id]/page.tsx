8f7f7ed2475ca7cd6908b695d7bbad27bbed90c3
Muhammed Sahil <89013603+sahil47ek@users.noreply.github.com>
2025-03-06 15:41:47 +0530

    Remove node_modules completely

--- src/app/products/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductDetails from '../../components/ProductDetails';
-
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <ProductDetails id={params.id} />;
-} 
